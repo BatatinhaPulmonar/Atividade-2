36b0a21828a114a57f21aacac1e746f009b036cf
eduardo <113903669+BatatinhaPulmonar@users.noreply.github.com>
2024-08-02 08:46:55 -0300

    Update MainProgram.cs

--- Atividade 2/ConsoleApp1/MainProgram.cs
+++ Atividade 2/ConsoleApp1/MainProgram.cs
@@ -6,8 +6,8 @@ class Program
 {
     static void Main()
     {
-        Cliente cliente = new Cliente("João Silva", "joao.silva@exemple.com");
-        Produto produto = new Produto("Agua");
+        Cliente cliente = new Cliente("Pedro", "pedro.hype@exemple.com");
+        Produto produto = new Produto("Chocolate");
 
         Console.WriteLine($"Cliente: {cliente.Nome} \nEmail: {cliente.Email} \nProduto: {produto.Produtos}");
     }
